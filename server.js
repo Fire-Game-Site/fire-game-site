202ac7612dc58fc1fbfdb170b6e9c2bff2884396
KwishyWishy <135928187+KwishyWishy@users.noreply.github.com>
2023-06-14 20:18:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var games = {
         title: "Papa's Freezeria", // type the title of the game here
         link: "papas-freezeria", // the title but with link friendly, so no spaces or symbols
         image: "https://th.bing.com/th/id/OIP.52nSt-_9qvnIJd4vJoOQqwHaEo?pid=ImgDet&rs=1", // type the link to the cover image here
-        embedLink: "https://paper-io.com/" // type the iframe embed link here
+        embedLink: "https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/1ee20621-61bc-4ec8-a8ec-5e839c2e6edc%2Fpapa-s-freezeria.xml" // type the iframe embed link here
     },
     "defly.io": {
         title: "Defly.io (All Games)",
@@ -21,7 +21,7 @@ var games = {
         title: "Paper.io 2",
         link: "paper.io-2",
         image: "https://img.poki.com/cdn-cgi/image/quality=78,width=600,height=600,fit=cover,f=auto/d2708e8aa31df3fe7b211bca36405d6d.png",
-        embedLink: "https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/r3tq6l1/to9d2if10dv@bc085da43274dbed133e925931d26accfded2b79/x6j98kl/d8g3s.xml"
+        embedLink: "https://paper-io.com/"
     },  
         "hexanaut.io": {
         title: "Hexanaut.io",
