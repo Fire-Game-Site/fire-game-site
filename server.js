0cac21fe96b935e075dd1a129ccf31e180334bd2
KwishyWishy <135928187+KwishyWishy@users.noreply.github.com>
2023-06-19 23:07:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ var games = {
         title: "Flap",
         link: "flap",
         image: "https://arsholde.sirv.com/Images/flap.jpeg?w=209&h=177&scale.option=ignore",
-        embedLink: "https://www.newgrounds.com/portal/view/749633"
+        embedLink: "https://files.acticdn.com/278374/30017/index.html?an_game_version=202009241022"
      "8ballpool": {
         title: "8 Ball Pool",
         link: "8ballpool",
