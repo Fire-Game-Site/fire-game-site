7404bfc69a61295f6ed9a66965cd7898f25b4112
KwishyWishy <135928187+KwishyWishy@users.noreply.github.com>
2023-11-22 23:12:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 10000
 
 var announcement = { // both support html, so if you want something like a link use <a> or if you want a newline use <br />
     title: "Anouncements",
-    description: "Make sure to SUBSCRIBE to our <a href='https://youtube.com/@firegames52123?si=tdwqa6Hx4Emqtfbb'>YouTube Channel!</a> <br />  <br />November 20, 2023: A fullscreen feature has been added! Make sure to check it out! <br />We are aware of an ongoing issue involving sizing of ads on game pages. We are currently looking into solutions and we are sorry for any inconvenience this may cause you. <br />  <br />New Games:<br /><ul><li><a href='/snowrider'>Snow Rider 3D</a></li><li><a href='/impossiblegame'>The Impossible Game</a></li><li><a href='/geometrydash'>Geometry Dash</a></li><li><a href='/99balls'>99 Balls</a></li><li><a href='/raftwars'>Raft Wars</a></li><li><a href='/fnaf'>FNAF</a></li></ul>"
+    description: "Make sure to SUBSCRIBE to our <a href='https://youtube.com/@firegames52123?si=tdwqa6Hx4Emqtfbb'>YouTube Channel!</a> <br />  <br />November 20, 2023: A fullscreen feature has been added! Make sure to check it out! <br />We are aware of an ongoing issue involving sizing of ads on game pages. We are currently looking into solutions and we are sorry for any inconvenience this may cause you. <br />  <br />New Games:<br /><ul><li><a href='/geometrydash'>Geometry Dash</a></li><li><a href='/99balls'>99 Balls</a></li><li><a href='/raftwars'>Raft Wars</a></li><li><a href='/raftwars2'>Raft Wars 2</a></li><li><a href='/fnaf'>FNAF</a></li><li><a href='/mrmine'>Mr Mine</a></li></ul>"
 }
 
 var games = {
