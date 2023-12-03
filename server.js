0e08a838e2cb02e10d9a7673e40e1bab7dcc0839
Hex <maxwellturtle@icloud.com>
2023-12-03 17:11:12 -0500

    Remove meilisearch.deb and remove sizing issue from announcement

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 10000
 
 var announcement = { // both support html, so if you want something like a link use <a> or if you want a newline use <br />
     title: "Anouncements",
-    description: "Make sure to SUBSCRIBE to our <a href='https://youtube.com/@firegames52123?si=tdwqa6Hx4Emqtfbb'>YouTube Channel!</a> <br />  <br />November 20, 2023: A fullscreen feature has been added! Make sure to check it out! <br />We are aware of an ongoing issue involving sizing of ads on game pages. We are currently looking into solutions and we are sorry for any inconvenience this may cause you. <br />  <br />New Games:<br /><ul><li><a href='/madalinstuntcars2'>Madalin Stunt Cars 2</a></li><li><a href='/tetris'>Tetris</a></li><li><a href='/penaltykickonline'>Penalty Kick Online</a></li><li><a href='/raftwars2'>Raft Wars 2</a></li><li><a href='/fnaf'>FNAF</a></li><li><a href='/mrmine'>Mr Mine</a></li></ul>"
+    description: "Make sure to SUBSCRIBE to our <a href='https://youtube.com/@firegames52123?si=tdwqa6Hx4Emqtfbb'>YouTube Channel!</a> <br />  <br />November 20, 2023: A fullscreen feature has been added! Make sure to check it out! <br />  <br />New Games:<br /><ul><li><a href='/madalinstuntcars2'>Madalin Stunt Cars 2</a></li><li><a href='/tetris'>Tetris</a></li><li><a href='/penaltykickonline'>Penalty Kick Online</a></li><li><a href='/raftwars2'>Raft Wars 2</a></li><li><a href='/fnaf'>FNAF</a></li><li><a href='/mrmine'>Mr Mine</a></li></ul>"
 }
 
 var games = {
